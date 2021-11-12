5e4d36e504c6b9dcefb45ece691fc0ec5385431f
farah <farah.barade@gmail.com>
2021-11-12 17:40:52 +0100

    liste quiz

--- lib/doquiz.dart
+++ lib/doquiz.dart
@@ -1,40 +1,85 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project_master/quiz.dart';
 
 class DoQuiz extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
     return new letsDoQuizs();
   }
+  Quiz quiz1 = new Quiz(1, "geoquiz");
+  Quiz quiz2 = new Quiz(2, "geoquiz");
+  Quiz quiz3 = new Quiz(3, "geoquiz");
+  Quiz quiz4 = new Quiz(4, "geoquiz");
+
+  List<Quiz> list_quizes = List<Quiz>[];
+  list_quizes.add(quiz1);
+
+  var myListQuiz = List.filled(4, null, growable: false);
+  myListQuiz;
 }
 class letsDoQuizs extends State<DoQuiz> {
 
+
+
+  void _remove(int index) {
+    setState(() {
+      _quizes.remove(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Quizer'),
+          title: const Text('Quizes'),
         ),
-        body: Center(child: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(150),
+        body:ListView.builder(
+        itemCount: _quizes.list_questions.length,
+        itemBuilder: (BuildContext context, int index) {
 
-              child: Text('', style: TextStyle(fontSize: 20.0),),
-
-            ),
-
-          Container(
-            //margin: EdgeInsets.all(10),
-            child: ElevatedButton(
-              child: Text('Gérer', style: TextStyle(fontSize: 20.0),),
-              onPressed: () {},
-            ),
-          ),
-        ]
-        ))
-
+         final item = _quizes.list_questions[index];
+         return Dismissible(
+       key: Key(item),
+        background: Container(
+        child: Icon(
+        Icons.delete,
+        size: 40,
+       color: Colors.white,
+        ),
+       color: Colors.cyan,
+        ),
+        onDismissed: (direction) {
+         _remove(index);
+         var snackBar = SnackBar(
+         content: Text('Suppression du grand site $item'),
+         action: SnackBarAction(
+         label: 'Annulation suppression',
+         onPressed: () {
+        setState(() {
+    _quizes.add(item);
+    });
+    }
+    )
     );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    },
+    child: Card(
+    child: ListTile(
+    title: Text(
+    _quizes.list_questions[index],
+    textAlign: TextAlign.center,
+    ),
+    onTap: () {
+    var snackBar =
+    SnackBar(content: Text('$item'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    })));
+    }),
+    }
+        )
+
+    
   }
 }
 
