550fdf0ad14e0e6ab897c0d87d2e0f075965faef
Nicolas BOEHRER <nboehrer@laposte.net>
2021-11-12 17:08:19 +0100

    update quiz

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,16 +2,23 @@ import 'package:flutter_project_master/question.dart';
 
 class Quiz {
    int _id = 1 ;
-   List<Question> _list_questions = <Question>[] ;
+   String _name;
 
-   Quiz(this._id){
+  List<Question> _list_questions = <Question>[] ;
+
+   Quiz(this._id,this._name){
      this.addAll();
    }
 
-  Quiz.createQuiz( this._id , this._list_questions );
+  Quiz.createQuiz( this._id , this._list_questions, this._name );
 
    int get id => _id;
    List<Question> get list_questions => _list_questions;
+   String get name => _name;
+
+   set name(String value) {
+     _name = value;
+   }
 
    set id(int value) {
      _id = value;
@@ -36,10 +43,10 @@ class Quiz {
    void addAll(){
      _list_questions.add(new Question.createQuestion4Options("Amazone", "Le fleuve le plus long du monde", "Nil", "Amazone", "Gange", "Danube"));
      _list_questions.add(new Question.createQuestion4Options("Amazone", "Le fleuve le plus gros débit du monde", "Gange", "Amazone", "Mississipi", "Congo"));
-     _list_questions.add(new Question.createQuestion4Options("Everest", "Le mont le plus haut du monde", "Mont-Blanc", "Amazone", "Gange", "Danube"));
-     _list_questions.add(new Question.createQuestion4Options("Amazone", "Le fleuve le plus long du monde", "Nil", "Amazone", "Gange", "Danube"));
-     _list_questions.add(new Question.createQuestion4Options("Amazone", "Le fleuve le plus long du monde", "Nil", "Amazone", "Gange", "Danube"));
-     _list_questions.add(new Question.createQuestion4Options("Amazone", "Le fleuve le plus long du monde", "Nil", "Amazone", "Gange", "Danube"));
+     _list_questions.add(new Question.createQuestion4Options("Everest", "Le mont le plus haut du monde", "Lhotse", "Everest", "Morond", "K2"));
+     _list_questions.add(new Question.createQuestion4Options("Danube", "Le fleuve le plus long d'Europe", "Volga", "Rhone", "Danube", "Po"));
+     _list_questions.add(new Question.createQuestion4Options("Pacifique", "L océan le plus grand du monde", "Indien", "Arctique", "Pacifique", "Atlantique"));
+     _list_questions.add(new Question.createQuestion4Options("Franche-Comte", "La plus belle région de France", "Bretagne", "Franche-Comte", "Limousin", "Centre"));
    }
 
 }
