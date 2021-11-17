a4898b53a038b1edbf84f3164cedd6de45d5978f
Nicolas BOEHRER <nboehrer@laposte.net>
2021-11-17 16:03:23 +0100

    Remove Edition Fonction from playquiz

--- lib/playquiz.dart
+++ lib/playquiz.dart
@@ -41,20 +41,6 @@ var _quizes = Quiz(1, "a");
           ),
           color: Colors.cyan,
         ),
-        onDismissed: (direction) {
-          _remove(index);
-          var snackBar = SnackBar(
-              content: Text('Suppression du grand site $item'),
-              action: SnackBarAction(
-                  label: 'Annulation suppression',
-                  onPressed: () {
-                    setState(() {
-                      _quizes.add(item);
-                    });
-                  })
-          );
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
         child: Card(
             child: ListTile(
                 title: Text(
@@ -67,29 +53,6 @@ var _quizes = Quiz(1, "a");
                   ScaffoldMessenger.of(context).showSnackBar(snackBar);
                 })));
     }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Ajouter un grand site'),
-                  content: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Saisissez le nom du grand site"),
-                      onSubmitted: (value) {
-                        setState(() {
-                          _quizes.add(value);
-                        });
-                        Navigator.of(context).pop();
-                      }),
-                );
-              });
-        },
-        tooltip: 'ajouter un grand site',
-        child: Icon(Icons.add),
-      ),
     );
 
 
